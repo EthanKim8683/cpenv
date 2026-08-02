fc8c4060a467b33c9d2b6be5a5342b2f7fc11e5c
Ethan Kim <ethankim8683@gmail.com>
2026-08-02 13:02:05 -0700

    Add back TODO

--- internal/server/focus.go
+++ internal/server/focus.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	focusv1 "github.com/EthanKim8683/cpenv/gen/focus/v1"
 	"github.com/EthanKim8683/cpenv/gen/focus/v1/focusv1connect"
@@ -10,10 +11,14 @@ import (
 )
 
 type FocusService struct {
+	mu         sync.Mutex
 	StateStore state.Store
 }
 
 func (s *FocusService) SetFocus(_ context.Context, req *focusv1.SetFocusRequest) (*focusv1.SetFocusResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	state, err := s.StateStore.Load()
 	if err != nil {
 		return nil, fmt.Errorf("set focus: %w", err)
