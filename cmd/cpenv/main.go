c367b38974a83a55e65cea46150453de05bd0d23
Ethan Kim <ethankim8683@gmail.com>
2026-08-01 22:14:55 -0700

    Fix double error message

--- cmd/cpenv/main.go
+++ cmd/cpenv/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/EthanKim8683/cpenv/cmd/cpenv/command"
@@ -9,7 +8,6 @@ import (
 
 func main() {
 	if err := command.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "cpenv: %v\n", err)
 		os.Exit(1)
 	}
 }
