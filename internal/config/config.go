a81ca5073b15566e0b41f87225c88d0a4b16629e
Ethan Kim <ethankim8683@gmail.com>
2026-08-01 22:10:49 -0700

    Make env vars more user-friendly

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,25 +11,49 @@ import (
 
 type Config struct {
 	Port          string `env:"CPENV_PORT" envDefault:"8080"`
-	StatePath     string `env:"CPENV_STATE_PATH,notEmpty"`
-	TemplatesDir  string `env:"CPENV_TEMPLATES_DIR,notEmpty"`
-	WorkspacesDir string `env:"CPENV_WORKSPACES_DIR,notEmpty"`
+	Home          string `env:"CPENV_HOME"`
+	StatePath     string `env:"CPENV_STATE_PATH"`
+	TemplatesDir  string `env:"CPENV_TEMPLATES_DIR"`
+	WorkspacesDir string `env:"CPENV_WORKSPACES_DIR"`
 }
 
 func (c *Config) Validate() error {
 	var errs error
+
 	if port, err := strconv.Atoi(c.Port); err != nil || !(1 <= port && port <= 65535) {
 		errs = errors.Join(errs, fmt.Errorf("CPENV_PORT %q must be an integer between 1 and 65535", c.Port))
 	}
-	if !filepath.IsAbs(c.StatePath) {
+
+	if c.Home != "" {
+		if !filepath.IsAbs(c.Home) {
+			errs = errors.Join(errs, fmt.Errorf("CPENV_HOME %q must be an absolute path", c.Home))
+		} else {
+			if c.StatePath == "" {
+				c.StatePath = filepath.Join(c.Home, "state.json")
+			}
+			if c.TemplatesDir == "" {
+				c.TemplatesDir = filepath.Join(c.Home, "templates")
+			}
+			if c.WorkspacesDir == "" {
+				c.WorkspacesDir = filepath.Join(c.Home, "workspaces")
+			}
+		}
+	}
+
+	if c.StatePath != "" && !filepath.IsAbs(c.StatePath) {
 		errs = errors.Join(errs, fmt.Errorf("CPENV_STATE_PATH %q must be an absolute path", c.StatePath))
 	}
-	if !filepath.IsAbs(c.TemplatesDir) {
+	if c.TemplatesDir != "" && !filepath.IsAbs(c.TemplatesDir) {
 		errs = errors.Join(errs, fmt.Errorf("CPENV_TEMPLATES_DIR %q must be an absolute path", c.TemplatesDir))
 	}
-	if !filepath.IsAbs(c.WorkspacesDir) {
+	if c.WorkspacesDir != "" && !filepath.IsAbs(c.WorkspacesDir) {
 		errs = errors.Join(errs, fmt.Errorf("CPENV_WORKSPACES_DIR %q must be an absolute path", c.WorkspacesDir))
 	}
+
+	if c.StatePath == "" || c.TemplatesDir == "" || c.WorkspacesDir == "" {
+		errs = errors.Join(errs, fmt.Errorf("CPENV_HOME or CPENV_STATE_PATH, CPENV_TEMPLATES_DIR, and CPENV_WORKSPACES_DIR must be set"))
+	}
+
 	return errs
 }
 
