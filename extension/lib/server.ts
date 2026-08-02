bc81442e9b454fd8e5a074139f9deadb89fa5e42
Ethan Kim <ethankim8683@gmail.com>
2026-08-01 22:48:31 -0700

    Add focus retries

--- extension/lib/server.ts
+++ extension/lib/server.ts
@@ -12,10 +12,22 @@ import {
 import { create, MessageInitShape } from "@bufbuild/protobuf";
 import { ProblemSchema } from "@/gen/problem/v1/problem_pb";
 import { FocusSchema } from "@/gen/focus/v1/focus_pb";
+import retry from "async-retry";
 
-const initialReconnectDelayMs = 500;
-const reconnectDelayGrowthFactor = 1.5;
-const maxReconnectDelayMs = 60_000;
+const retryOpts = {
+  forever: true,
+  factor: 2,
+  minTimeout: 500,
+  maxTimeout: 60_000,
+  randomize: false,
+  onRetry: (error: unknown, attempt: number) => {
+    const timeout = Math.min(
+      retryOpts.minTimeout * retryOpts.factor ** (attempt - 1),
+      retryOpts.maxTimeout,
+    );
+    console.warn(`Disconnected from server; retrying in ${timeout}ms`, error);
+  },
+};
 
 const transport = createConnectTransport({
   baseUrl: `http://localhost:${import.meta.env.WXT_PORT}`,
@@ -47,56 +59,54 @@ export function createProblemMain({
       };
     }
 
+    let currentEventId = 0;
     const handleVisibilityChange = async () => {
       if (document.visibilityState !== "visible") return;
-      await focusClient.setFocus(
-        create(SetFocusRequestSchema, {
-          focus,
-        }),
-      );
+
+      currentEventId++;
+      const eventId = currentEventId;
+
+      await retry<void, void>(async (bail) => {
+        if (currentEventId !== eventId) {
+          bail();
+          return;
+        }
+
+        await focusClient.setFocus(
+          create(SetFocusRequestSchema, {
+            focus,
+          }),
+        );
+      }, retryOpts).catch();
     };
     handleVisibilityChange();
     document.addEventListener("visibilitychange", handleVisibilityChange);
 
-    let reconnectDelay = initialReconnectDelayMs;
-    while (true) {
-      try {
-        for await (const {
-          callbackId,
-          fileName,
-          content,
-        } of submitClient.subscribe(
-          create(SubscribeRequestSchema, {
-            problemId: getProblemId(),
-          }),
-        )) {
-          reconnectDelay = initialReconnectDelayMs;
-
-          let error: string | undefined;
-          try {
-            await submit(new File([new Uint8Array(content)], fileName));
-          } catch (caughtError: unknown) {
-            error = String(caughtError);
-          }
-          await submitClient.callback(
-            create(CallbackRequestSchema, {
-              callbackId,
-              error,
-            }),
-          );
+    await retry(async () => {
+      for await (const {
+        callbackId,
+        fileName,
+        content,
+      } of submitClient.subscribe(
+        create(SubscribeRequestSchema, {
+          problemId: getProblemId(),
+        }),
+      )) {
+        let error: string | undefined;
+        try {
+          await submit(new File([new Uint8Array(content)], fileName));
+        } catch (caughtError: unknown) {
+          error = String(caughtError);
         }
-      } catch (error: unknown) {
-        console.warn(
-          `Disconnected from server; reconnecting in ${reconnectDelay}ms`,
-          error,
+        await submitClient.callback(
+          create(CallbackRequestSchema, {
+            callbackId,
+            error,
+          }),
         );
       }
 
-      await new Promise((resolve) => setTimeout(resolve, reconnectDelay));
-      reconnectDelay = Math.min(
-        reconnectDelay * reconnectDelayGrowthFactor,
-        maxReconnectDelayMs,
-      );
-    }
+      throw new Error("Disconnected gracefully.");
+    }, retryOpts);
   };
 }
